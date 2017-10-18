5f8121c6eb0d1495c8388ebde8ced858a8c3a854
Naoki Hiroshima <naoki.hiroshima@gmail.com>
2017-10-18 13:03:18 -0700

    Fix package for Carthage.

---
+++ Sources/TypedUserDefaults/TypedUserDefaults.h
@@ -0,0 +1,19 @@
+//
+//  TypedUserDefaults.h
+//  TypedUserDefaults
+//
+//  Created by Naoki Hiroshima on 10/18/17.
+//  Copyright © 2017 Naoki Hiroshima. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TypedUserDefaults.
+FOUNDATION_EXPORT double TypedUserDefaultsVersionNumber;
+
+//! Project version string for TypedUserDefaults.
+FOUNDATION_EXPORT const unsigned char TypedUserDefaultsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TypedUserDefaults/PublicHeader.h>
+
+
